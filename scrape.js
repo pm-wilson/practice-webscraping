ed65ac36755ab32e013540da9f13bcdffd8e045b
Patrick Wilson <pmwilson333@gmail.com>
2020-09-15 15:33:47 -0700

    web scraper working for full site

--- scrape.js
+++
@@ -1,9 +0,0 @@
-const request = require('./lib/request');
-const parse = require('./lib/parser');
-const store = require('./lib/store');
-
-request()
-  .then(document => parse(document))
-  .then(books => store(books))
-  .then(books => console.log(books.length));
- 
