a7afb1d23ae9302463b76d779e3e752a277e2b61
Patrick Wilson <pmwilson333@gmail.com>
2020-09-15 16:10:37 -0700

    tests passing

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -2,13 +2,13 @@ const request = require('../lib/request');
 const parser = require('../lib/parser');
 
 describe('parser function', () => {
-  it('returns an array of all book image, rating, title, price, in stock', async() => {
-    const document = await request();
+  it('returns an array of all book image, rating, title, price, in stock', async () => {
+    const document = await request(1);
     const bookInfo = parser(document);
 
     expect(bookInfo).toEqual(expect.arrayContaining([
       { 
-        image: 'http://books.toscrape.com/media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg', 
+        image: 'http://books.toscrape.com/../media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg', 
         rating: 3, 
         title: 'A Light in the Attic', 
         price: '£51.77', 
